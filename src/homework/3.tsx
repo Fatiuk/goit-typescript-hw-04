0a8a99fae892fb5ca6f19688b48e74c9e6617144
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-11-05 19:54:15 +0200

    feat: complete task 3

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 export function FormComponent() {
   const [value, setValue] = useState("");
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
