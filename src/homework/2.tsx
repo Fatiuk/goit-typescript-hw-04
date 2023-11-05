dddc8b677532e244ad8a5d28091635fb267e9eec
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-11-05 19:51:04 +0200

    feat: complete task 2

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,9 +1,22 @@
 import React, { useReducer } from "react";
 
+type RequestStep = "idle" | "start" | "pending" | "finished";
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+};
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: "idle",
 };
+
+type Action =
+  | { type: "START_REQUEST" }
+  | { type: "PENDING_REQUEST" }
+  | { type: "FINISH_REQUEST" }
+  | { type: "RESET_REQUEST" };
 
 function requestReducer(state: State, action: Action): State {
   switch (action.type) {
